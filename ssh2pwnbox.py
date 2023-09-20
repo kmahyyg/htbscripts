df6726d9979ec7372576ac88fea8194dd81ad6e3
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2023-09-20 21:32:01 +0800

    update script

--- ssh2pwnbox.py
+++ ssh2pwnbox.py
@@ -7,12 +7,15 @@ import subprocess
 import json
 import shutil
 import os
+import urllib.parse as uparse
+import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-o", dest='op', type=str, choices=["save", "connect", "reset", "show"], default="connect", help="operation")
 parser.add_argument("-d", dest='dst', type=str, required=False, help="pwnbox ip address")
 parser.add_argument("-u", dest="user", type=str, required=False, help="pwnbox username")
 parser.add_argument("-p", dest='passwd', type=str, required=False, help="pwnbox ssh password")
+parser.add_argument("-s", dest='furl', type=str, required=False, help="full pwnbox url")
 cmdargs = parser.parse_args()
 
 def main():
@@ -25,12 +28,26 @@ def main():
     pwnboxIPFile = os.path.expandvars('${HOME}/.config/pwnbox-ip.json')
     # store
     if cmdargs.op == "save":
-        if cmdargs.user == "" or cmdargs.passwd == "" or cmdargs.dst == "":
-            raise AssertionError("user, dest ip, password are required. port is always 22.")
-        pwnboxIPstr = cmdargs.dst
+        if cmdargs.user == "" or (cmdargs.passwd == "" and cmdargs.dst == "") or cmdargs.furl == "":
+            raise AssertionError("user, (dest ip, password)/(full url of viewer) are required. port is always 22.")
+        pwnboxIPstr = ""
+        pwnboxPwd = cmdargs.passwd
+        if cmdargs.dst != "":
+            pwnboxIPstr = cmdargs.dst
+        if cmdargs.furl != "":
+            u1 = uparse.urlparse(cmdargs.furl)
+            if u1.netloc != "vnc.htb-cloud.com":
+                raise AssertionError("Only HTB Viewer Full URL is supported.")
+            u2 = dict(uparse.parse_qsl(u1.query))
+            pwnboxPwd = u2["password"]
+            r1 = u2["host"].index("bird/")
+            pwnboxIPstr = u2["host"][r1 + len("bird/"):]
+        if pwnboxPwd == "":
+            raise SystemError("cannot parse url to retrive data")
+        print("Host: ", pwnboxIPstr, " ,Password: ", pwnboxPwd)
         with open(pwnboxIPFile, "w") as fd:
             fd.write(json.dumps({"user": cmdargs.user, "dstip": pwnboxIPstr}))
-        keyring.set_password(pwnboxIPstr, cmdargs.user, cmdargs.passwd)
+        keyring.set_password(pwnboxIPstr, cmdargs.user, pwnboxPwd)
         print("done!")
         return
     # Prestart check
@@ -58,8 +75,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# this application will help you quickly connect to pwnbox from your local machine, and save your password in your current pc's keychain
-# to use: after op==save, try `eval $(ssh2pwnbox)`. When pwnbox terminated, do op==reset
-# to debug: op==show
-# tested on MacOS
